73bdf0388fe2618b551fcdbb2b2327aefb2ee702
20SuperNOVA04 <89562721+20SuperNOVA04@users.noreply.github.com>
2024-09-12 12:33:45 +1200

    Assignment 1 code model

--- Assignment_1/heat_flow_through_plate_model.py
+++ Assignment_1/heat_flow_through_plate_model.py
@@ -21,13 +21,12 @@ d = 0.1     #depth of support plate
 
 def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
     """Creates the matrix A containing how each cell conducts heat, and the corresponding vector b with the constants of the heat transfer"""
-     
     #creating a zero matrix and vector for values to be set into
     A = np.zeros((N*M,N*M), float)
     b = np.zeros((N*M,1), float)
     #setting a general offset for each iteration
     offset = 0
-    
+        
     #top left corner node
     A[offset][offset] = -3-h_left*dx/k
     A[offset][offset+1] = 1
@@ -35,7 +34,6 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
 
     b[offset] = -(h_left*dx/k)*T_air-T_top
     offset += 1
-    
     
     #top edge nodes (excluding corners)
     for i in range(0, M-2):
@@ -46,8 +44,7 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
         
         b[offset] = -T_top
         offset+=1
-
-
+        
     #top right coner node
     A[offset][offset-1] = 1
     A[offset][offset] = -3-h_right*dx/k
@@ -55,7 +52,7 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
     
     b[offset] = -(h_right*dx/k)*T_air-T_top
     offset+=1
-    
+
     
     #looping through each row
     for x in range(0, N-2):
@@ -68,7 +65,6 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
         b[offset] = -(h_left*dx/k)*T_air
         offset+=1
         
-        
         for y in range(0, M-2):
             #internal node
             A[offset][offset-M] = 1
@@ -78,7 +74,6 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
             A[offset][offset+M] = 1
             offset+=1
             
-            
         #right edge node
         A[offset][offset-M] = 1
         A[offset][offset-1] = 1
@@ -87,7 +82,6 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
         
         b[offset] = -(h_right*dx/k)*T_air
         offset+=1
-        
     
     #bottom left corner node
     A[offset][offset-M] = 1
@@ -113,7 +107,6 @@ def define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k):
     A[offset][offset] = -3 - h_right*dx/k
     
     b[offset] = -(h_right*dx/k)*T_air-T_bot
-        
     return(A, b)
 
 def create_temp_matrix(A, b, N, M):
@@ -121,10 +114,8 @@ def create_temp_matrix(A, b, N, M):
     #solving for the temperature of each cell
     temp_vector = alg.solve(A, b)
     temp_vector = np.flip(temp_vector)
-
     #putting each temperature into matrix form
     temp_matrix = np.zeros((N,M), float)
-
     i = 0
     #looping through each temperature in the temp_vector and putting it in the temp_matrix
     #where each value is put in each row and wraps to next rows.
@@ -139,22 +130,18 @@ def plot_heatmap(temp_matrix, N, M):
     
     #setting up grids to define where each cell sits in the heatmap
     X_grid,Y_grid = np.meshgrid(range(0,M), range(0,N))
-
     X_grid = dx/2+dx*X_grid
     Y_grid = Y-dy/2+dy*Y_grid
 
     #plotting the heatmap
     axs = plt.axes()
-
     heatmap = plt.pcolor(X_grid, Y_grid, temp_matrix, edgecolors='k', linewidths=0.25, shading = 'auto')
     plt.colorbar(heatmap)
     axs.set_aspect("equal")
     plt.show()
 
 def heat_conducted_from_side(d, k, T_row, T_side, M):
-    """Calculates the heat transfer to the support plate on the top row (in contact with the hot furnace wall) from the top furnace wall"""
-
-    
+    """Calculates the heat transfer to the support plate on the top row (in contact with the hot furnace wall) from the top furnace wall""" 
     #loops through each node in the top row and adds togeather the Q_dot values
     Q_dot = 0
     for i in range(0, M):
@@ -181,24 +168,18 @@ def heat_convected_to_air(d, h_left, h_right, T_matrix, T_air, N, M, dy):
 def run_program():
     #returns matrix A and vector b
     A, b = define_A_b(N, M, T_air, T_top, T_bot, h_left, h_right, k)
-
     #returns A and b to get temperatures and forms it into a matrix
     temp_matrix = create_temp_matrix(A,b, N, M)
-
     #returns the heat transfer from the top furnace wall to the cells in contact with the top wall
     Q_dot_of_top_plate = heat_conducted_from_side(d, k, temp_matrix[N-1],T_top, M)
-
     #returns the heat transfer from the bottom furnace wall to the cells in contact with the bottom wall
     Q_dot_of_bottom = heat_conducted_from_side(d, k, temp_matrix[0],T_bot, M)
-    
     #returns the heat transfer from the cells in contact with air to the air
     Q_dot_of_left, Q_dot_right = heat_convected_to_air(d, h_left,h_right, temp_matrix, T_air, N, M, dy)
-    
     return Q_dot_of_top_plate, Q_dot_of_bottom, Q_dot_of_left, Q_dot_right
 
 #Allow anyone to select what they want to see/load (makes the plotting not intrusive when you dont want it :D)
 to_see = " "
-
 #allow user to set the h left and right values
 h_left = int(input("\nWhat is the left h value: "))     #thermal conductivity coefficient for the left side
 h_right = int(input("What is the right h value: "))     #thermal conductivity coefficient for the right side
@@ -222,18 +203,14 @@ Leave Empty to Shutdown: """)
     if to_see == "r":
         h_left = int(input("\nWhat is the left h value: "))     #thermal conductivity coefficient for the left side
         h_right = int(input("What is the right h value: "))     #thermal conductivity coefficient for the right side
-    
     #prints the value of the heat transfer from the top furnace wall into the plate
     if to_see == "t":
         print(f"Heat Transfer via conduction from top furnace wall into support plate is {Q_dot_of_top_plate:.2F}.")
-
     #prints the value of the heat transfer from the plate into the bottom furnace wall
     elif to_see == "b":
         print(f"Heat Transfer via conduction from the bottom furnace wall to the support plate is {Q_dot_of_bottom:.2F}.")
-
     elif to_see == "s":        
         print(f"Heat Transfer via convection from the support plate into the air is {Q_dot_of_left:.2F} on the left side and {Q_dot_right:.2F} on the right. Summing to {Q_dot_of_left+Q_dot_right:.2F}.")
-    #high difference between T of the plate column nodes and T_air, so high heat transfer
     
     #find and print the correct steady state value of Q_dot_sides
         print(f"The steady state value of Heat transfer via convection from the support plate into the air is {Q_dot_of_top_plate+Q_dot_of_bottom:.2F} total.")
@@ -255,6 +232,3 @@ Leave Empty to Shutdown: """)
             if Q_dot_of_bottom > 0:
                 break
         print(f"The lowest whole value of h where heat is transfered from the lower temperature furnace wall is {i}.")
-
-
-
